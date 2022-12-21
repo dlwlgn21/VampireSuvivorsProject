04f44c2918117e64bbda73a7760e3e0d21d41558
dlwlgn213 <dlwlgn213@gmail.com>
2022-12-21 13:40:21 +0900

    Time 클래스에 Stop, Resume, TotalTime 기능 추가. 이제 Time이 Stop될때에 모든 기능 멈추게 만들어야 함

--- yaTime.cpp
+++ yaTime.cpp
@@ -3,36 +3,78 @@
 
 namespace ya
 {
-
 	LARGE_INTEGER Time::mCPUFrequency;
-	LARGE_INTEGER Time::mPrevFrequency;
-	LARGE_INTEGER Time::mCurFrequency;
 	float Time::mDeltaTime = 0.0f;
-	float Time::mTime = 0.0f;
+	float Time::mFpsTimer = 0.0f;
 	HWND Time::mHwnd;
+
+	float Time::mScale = 1.0f;
+	double Time::mdSecondPerCount{ 0.0 };
+	long long Time::mllBaseTime{ 0 };
+	long long Time::mllPausedTime{ 0 };
+	long long Time::mllStopTime{ 0 };
+	long long Time::mllPrevTime{ 0 };
+	long long Time::mllCurrTime{ 0 };
+	bool Time::mbIsStoped = false;
+
 	void Time::Initialize()
 	{
+		// 시스템의 고성능 카운터는 주파수 형태로 값을 반환한다. 하지만 우리에게 필요한 것은 시간!
+		// 따라서, 주파수(Hz)형태를 구해와서 역으로 1 Cycle 당 경과 시간을 구해둔다.
+		// 이 값은 변하지 않는 값으로 앞으로 구해올 주파수에 이 값을 곱하면, 원하는 시간(초)를 구할 수 있게 됨.
+		
 		QueryPerformanceFrequency(&mCPUFrequency);			// CPU 초당 반복 되는 진동수를 얻어오는 함수 처음엔 0임.
-		QueryPerformanceCounter(&mPrevFrequency);			// 프로그램 시작 했을 때에 CPU의 클럭수 
+
+
+		//QueryPerformanceCounter(&mPrevFrequency);			// 프로그램 시작 했을 때에 CPU의 클럭수 
+		
+		// ADD PART
+		mdSecondPerCount = 1.0 / static_cast<double>(mCPUFrequency.QuadPart);
+		
+		long long currTime;
+		QueryPerformanceCounter(reinterpret_cast<LARGE_INTEGER*>(&currTime));
+
+		mllBaseTime = currTime;
+		mllPrevTime = currTime;
+		mllStopTime = 0;
+		mbIsStoped = false;
+		// ADD PART END
+
 		mHwnd = Application::GetInstance().GetWindowData().hwnd;
+
+
 	}
 
 	void Time::Tick()
 	{
-		QueryPerformanceCounter(&mCurFrequency);
+		//QueryPerformanceCounter(&mCurFrequency);
+		//float differntFrequency
+		//	= static_cast<float>(mCurFrequency.QuadPart - mPrevFrequency.QuadPart);
+		//mDeltaTime = differntFrequency / mCPUFrequency.QuadPart;
+		//mPrevFrequency.QuadPart = mCurFrequency.QuadPart;	// 갱신
 
-		float differntFrequency
-			= static_cast<float>(mCurFrequency.QuadPart - mPrevFrequency.QuadPart);
+		// ADD PART
+		if (mbIsStoped)
+		{
+			mDeltaTime = 0.0f;
+			return;
+		}
 
-		mDeltaTime = differntFrequency / mCPUFrequency.QuadPart;
+		long long currTime;
+		QueryPerformanceCounter(reinterpret_cast<LARGE_INTEGER*>(&currTime));
 
-
-		mPrevFrequency.QuadPart = mCurFrequency.QuadPart;	// 갱신
+		mllCurrTime = currTime;
+		mDeltaTime = static_cast<float>((mllCurrTime - mllPrevTime) * mdSecondPerCount);
+		mllPrevTime = mllCurrTime;
+		if (mDeltaTime < 0.0f)
+		{
+			mDeltaTime = 0.0f;
+		}
 	}
 
 	void Time::Render(HDC hdc)
 	{
-		mTime += Time::DeltaTime();
+		mFpsTimer += Time::DeltaTime();
 		//if (mTime > 1.0f)
 		//{
 		//	wchar_t buffer[64] = {};
@@ -47,17 +89,64 @@ namespace ya
 		//	//SetWindowTextW(mHwnd, buffer);
 		//	mTime = 0.0f;
 		//}
-		wchar_t buffer[64] = {};
+		if (mFpsTimer >= 0.1f)
+		{
+			wchar_t buffer[64] = {};
+			// DeltaTime == 한 프레임 도는데 걸린 시간.
+			float fps = 1.0f / mDeltaTime;
+			swprintf_s(buffer, 64, L"FramePerSecond : %.0f", fps);
+			size_t strLen = wcsnlen_s(buffer, 64);
+			TextOutW(hdc, 10, 10, buffer, static_cast<int>(strLen));
 
-		// DeltaTime == 한 프레임 도는데 걸린 시간.
-		float fps = 1.0f / mDeltaTime;
-		swprintf_s(buffer, 64, L"FramePerSecond : %.0f", fps);
-		size_t strLen = wcsnlen_s(buffer, 64);
-		TextOutW(hdc, 10, 10, buffer, static_cast<int>(strLen));
-		HWND hWnd
-			= Application::GetInstance().GetWindowData().hwnd;
-		//SetWindowTextW(mHwnd, buffer);
-		mTime = 0.0f;
+			swprintf_s(buffer, 64, L"Total Time == %.0f", TotalTime());
+			strLen = wcsnlen_s(buffer, 64);
+			TextOutW(hdc, 30, 30, buffer, static_cast<int>(strLen));
+			//HWND hWnd
+			//	= Application::GetInstance().GetWindowData().hwnd;
+			//SetWindowTextW(mHwnd, buffer);
+			mFpsTimer = 0.0f;
+		}
+	}
+
+	void Time::Stop()
+	{
+		if (!mbIsStoped)
+		{
+			long long currTime;
+			QueryPerformanceCounter(reinterpret_cast<LARGE_INTEGER*>(&currTime));
+
+			mllStopTime = currTime;
+			mbIsStoped = true;
+		}
+	}
+
+	void Time::Resume()
+	{
+		long long startTime;
+		QueryPerformanceCounter(reinterpret_cast<LARGE_INTEGER*>(&startTime));
+
+		if (mbIsStoped)
+		{
+			mllPausedTime += (startTime - mllStopTime);
+
+			mllPrevTime = startTime;
+			mllStopTime = 0;
+			mbIsStoped = false;
+		}
+
+	}
+
+	float Time::TotalTime()
+	{
+		if (mbIsStoped)
+			{ return static_cast<float>(((mllStopTime - mllPausedTime) - mllBaseTime) * mdSecondPerCount); }
+		else
+			{ return static_cast<float>(((mllCurrTime - mllPausedTime) - mllBaseTime) * mdSecondPerCount); }
+	}
+
+	void Time::SetScale(float scale)
+	{
+		mScale = scale;
 	}
 
 }
--- yaTime.h
+++ yaTime.h
@@ -10,16 +10,33 @@ namespace ya
 		static void Tick();
 		static void Render(HDC hdc);
 
-		inline static float DeltaTime() { return mDeltaTime; }
+		static void Stop();
+		static void Resume();
+		static float TotalTime();
+		static void SetScale(float scale);
+
+
+		__forceinline static float DeltaTime() { return mDeltaTime * mScale; }
 
 	private:
 		// CPU 고유 진동수 ( 1초당 몇 번 )
 		static LARGE_INTEGER mCPUFrequency;		// CPU 진동수
-		static LARGE_INTEGER mPrevFrequency;		// 내 이전 프레임 진동수
-		static LARGE_INTEGER mCurFrequency;		// 내 현재 프레임 진동수
 		static HWND mHwnd;
 		static float mDeltaTime;
-		static float mTime;
+		static float mFpsTimer;
+
+		// 추가된 파트
+		static float mScale;
+		static double mdSecondPerCount;
+
+
+		static long long mllBaseTime;		// 기준 시간으로 게임이 처음 구동되었을 때의 시간
+		static long long mllPausedTime;		// 멈춰있었던 시간. 즉, Stop()을 호출한 후, Resume()을 호출할 때까지 걸린 시간
+		static long long mllStopTime;		// 멈춘 시간. 즉, Stop()을 호출했을 때의 시간
+		static long long mllPrevTime;		// 이전 시간. 즉, 이전 Tick()의 시간.
+		static long long mllCurrTime;		// 현재 시간. 즉, 현재 Tick()의 시간.
+
+		static bool mbIsStoped;
 	};
 }
 
